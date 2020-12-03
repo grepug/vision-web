51ad7235183b84e6bb7882225e315f177ef4d0e3
Kai Shao <grepug@icloud.com>
2020-12-03 11:34:45 +0800

    fix()

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const { config } = require("dotenv");
+
+const { parsed: DEV } = config({ path: ".env.development" });
+
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
@@ -7,5 +11,13 @@ module.exports = {
     });
 
     return config;
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: DEV.NEXT_PUBLIC_GRAPHQL_URI,
+        destination: DEV.NEXT_PUBLIC_REAL_GRAPHQL_URI,
+      },
+    ];
   },
 };
